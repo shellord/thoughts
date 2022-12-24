b94fad29e53d4f1c363b57d26f4e847ecb627cbf
Saheen Shoukath <saheenshoukath@gmail.com>
2022-12-24 22:15:22 +0530

    refactor: remove console logs

--- app/src/screens/activity-feed/ActivityFeedScreen.tsx
+++ app/src/screens/activity-feed/ActivityFeedScreen.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import {Text} from 'react-native';
 import Container from '~/components/ui/Container';
 import type {RootStackNavigationProp} from '~/navigators/types';
-import {useCurrentUserQuery} from '~/generated/graphql';
 
 const ActivityFeedScreen: React.FC<
   RootStackNavigationProp<'ActivityFeed'>
 > = () => {
-  const {data} = useCurrentUserQuery();
-  console.log(data);
-
   return (
     <Container>
       <Text>ActivityFeedScreen</Text>
